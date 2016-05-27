cb16ff4e11f1f804ffe48b05fceea7ad68b8a76f
rowysock <rowysock@gmail.com>
2016-05-27 17:36:57 +0200

    JSLint

--- Server.js
+++ Server.js
@@ -1,3 +1,5 @@
+/*jslint node: true, nomen: true*/
+'use strict';
 var express = require("express");
 var app = express();
 var router = express.Router();
@@ -8,7 +10,7 @@ var querystring = require('querystring');
 var iconv = require('iconv-lite');
 var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
 var config = require('./config');
-app.set('view engine', 'jade')
+app.set('view engine', 'jade');
 
 function getToken(callback) {
     var authOptions = {
@@ -24,7 +26,7 @@ function getToken(callback) {
 
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             var access_token = body.access_token;
             console.log(access_token);
@@ -34,13 +36,13 @@ function getToken(callback) {
     });
 }
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     console.log("/" + req.method);
     next();
 });
 
-router.get("/", function(req, res) {
-    getToken(function(access_token) {
+router.get("/", function (req, res) {
+    getToken(function (access_token) {
         var authOptions = {
             url: 'https://api.spotify.com/v1/me/playlists',
             headers: {
@@ -48,29 +50,29 @@ router.get("/", function(req, res) {
             },
             json: true
         };
-        request.get(authOptions, function(error, response, body) {
+        request.get(authOptions, function (error, response, body) {
             console.log(body);
             res.render('playlists', body);
         });
-    })
+    });
 });
 
-router.get("/spotify", function(req, res) {
+router.get("/spotify", function (req, res) {
 
-    var scopes = 'user-read-private user-read-email playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative'
+    var scopes = 'user-read-private user-read-email playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative';
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
-            configclient_id: configclient_id,
+            client_id: config.client_id,
             scope: scopes,
             redirect_uri: redirect_uri
         }));
 });
 
-router.get("/songs", function(req, res) {
-    var requestURL='https://api.spotify.com/v1/users/'+req.query.userid+'/playlists/'+req.query.id+'/tracks'
+router.get("/songs", function (req, res) {
+    var requestURL = 'https://api.spotify.com/v1/users/' + req.query.userid + '/playlists/' + req.query.id + '/tracks';
     console.log(requestURL);
-    getToken(function(access_token) {
+    getToken(function (access_token) {
         var authOptions = {
             url: requestURL,
             headers: {
@@ -78,22 +80,22 @@ router.get("/songs", function(req, res) {
             },
             json: true
         };
-        request.get(authOptions, function(error, response, body) {
+        request.get(authOptions, function (error, response, body) {
             res.render('songs', body);
         });
-    })
+    });
 });
 
-router.get("/style.css", function(req, res) {
+router.get("/style.css", function (req, res) {
     res.sendFile(path + "style.css");
 });
 
-router.get("/callback2", function(req, res) {
+router.get("/callback2", function (req, res) {
     res.sendFile(path + "contact.html");
     console.log(req.query);
 });
 
-router.get("/callback", function(req, res) {
+router.get("/callback", function (req, res) {
     res.sendFile(path + "contact.html");
     console.log(req.query);
     var authOptions = {
@@ -109,7 +111,7 @@ router.get("/callback", function(req, res) {
         json: true
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
         console.log(body);
         console.log(error);
         console.log(response);
@@ -121,10 +123,10 @@ router.get("/callback", function(req, res) {
 
 app.use("/", router);
 
-app.use("*", function(req, res) {
+app.use("*", function (req, res) {
     res.sendFile(path + "404.html");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Live at Port 3000");
 });
