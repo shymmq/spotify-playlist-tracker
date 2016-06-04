cd2247f569be97e000e6eac0d82bee82f40577f5
Szymek <szymek.wysocki@gmail.com>
2016-06-04 23:25:01 +0200

    Cleanup

--- Server.js
+++ Server.js
@@ -3,45 +3,17 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
-var path = __dirname + '/views/';
-var request = require('request');
 var Promise = require('promise');
-var url = require('url');
-var querystring = require('querystring');
 var MongoClient = require('mongodb').MongoClient;
 var config = require('./config');
 var spotify = require('./spotify');
 var loader = require('./loader');
 
-var redirect_uri = 'http://localhost:3000/callback';
-
-app.set('view engine', 'jade');
-
 //loader.load();
-
-function appendUsername(playlist) {
-    return spotify.api.getUser(playlist.owner.id).then(function (response) {
-        playlist.owner.display_name = response.body.display_name;
-        return playlist;
-    });
-}
+app.use(express.static(__dirname + '/public')); //serve static assets
 router.get("/", function (req, res) {
     // res.render("search");
     res.sendFile(__dirname + '/public/index.html');
-});
-router.get("/playlists", function (req, res) {
-    // res.sendFile(path + "404.html");
-    console.log("Starting retrieving playlists");
-    spotify.refreshAccessToken.then(function () {
-        return spotify.api.getUserPlaylists(config.user_id);
-    }).then(function (response) {
-        return Promise.all(response.body.items.map(appendUsername));
-    }).then(function (playlists) {
-        // res.render('playlists',{items: playlists});
-        res.json({
-            items: playlists
-        });
-    });
 });
 router.get("/tracks", function (req, res) {
     spotify.refreshAccessToken
@@ -90,56 +62,17 @@ router.get("/history", function (req, res) {
             console.log(err);
         });
 });
-app.use(express.static(__dirname + '/public')); //serve static assets
-app.use(express.static(__dirname + '/node_modules'));
+
 router.use(function (req, res, next) {
     console.log("/" + req.method);
     next();
 });
 
-router.get("/spotify", function (req, res) {
-
-    var scopes = 'user-read-private user-read-email playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative';
-    res.redirect('https://accounts.spotify.com/authorize?' +
-        querystring.stringify({
-            response_type: 'code',
-            client_id: config.client_id,
-            scope: scopes,
-            redirect_uri: redirect_uri
-        }));
-});
-
-router.get("/style.css", function (req, res) {
-    res.sendFile(path + "style.css");
-});
-
-router.get("/callback", function (req, res) {
-    res.sendFile(path + "contact.html");
-    console.log(req.query);
-    var authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        form: {
-            code: req.query.code,
-            redirect_uri: 'http://localhost:3000/callback',
-            grant_type: 'authorization_code'
-        },
-        headers: {
-            'Authorization': 'Basic ' + (new Buffer(config.client_id + ':' + config.client_secret).toString('base64'))
-        },
-        json: true
-    };
-
-    request.post(authOptions, function (error, response, body) {
-        console.log(body);
-        console.log(error);
-        console.log(response);
-    });
-});
-
 app.use("/", router);
 
 app.use("*", function (req, res) {
-    res.sendFile(path + "404.html");
+    res.status(404);
+    res.sendFile(__dirname + "/public/404.html");
 });
 
 app.listen(3000, function () {
