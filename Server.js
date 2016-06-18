c27f0d6bd6ef686df878434a436e3894027d7b6a
rowysock <rowysock@gmail.com>
2016-06-18 23:43:06 +0200

    Store load status in db & Eslint

--- Server.js
+++ Server.js
@@ -1,213 +1,209 @@
-/*jslint node: true, nomen: true*/
-'use strict';
-var express = require("express");
-var app = express();
-var router = express.Router();
-var Promise = require('promise');
-var MongoClient = require('mongodb').MongoClient;
+/* jslint node: true, nomen: true*/
+'use strict'
+var express = require('express')
+var app = express()
+var router = express.Router()
+var MongoClient = require('mongodb').MongoClient
+var config = require('./config')
+var spotify = require('./spotify')
+var loader = require('./loader')
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var methodOverride = require('method-override')
+var session = require('express-session')
+var passport = require('passport')
+var SpotifyStrategy = require('./node_modules/passport-spotify/lib/passport-spotify/index').Strategy
+var userTokens = []
+var path = require('path')
 
-var config = require('./config');
-var spotify = require('./spotify');
-var loader = require('./loader');
-
-var bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
-    methodOverride = require('method-override'),
-    session = require('express-session'),
-    passport = require('passport'),
-    SpotifyStrategy = require('./node_modules/passport-spotify/lib/passport-spotify/index').Strategy;
-var userTokens = [];
 passport.serializeUser(function (user, done) {
-    done(null, user);
-});
+  done(null, user)
+})
 
 passport.deserializeUser(function (obj, done) {
-    done(null, obj);
-});
+  done(null, obj)
+})
 
 passport.use(new SpotifyStrategy({
-        clientID: config.client_id,
-        clientSecret: config.client_secret,
-        callbackURL: config.app_url + '/callback'
-    },
+  clientID: config.client_id,
+  clientSecret: config.client_secret,
+  callbackURL: config.app_url + '/callback'
+},
     function (accessToken, refreshToken, profile, done) {
-        process.nextTick(function () {
-            userTokens[profile.id] = refreshToken;
-            return done(null, profile);
-        });
-    }));
-app.use(cookieParser());
-app.use(bodyParser());
-app.use(methodOverride());
+      process.nextTick(function () {
+        userTokens[profile.id] = refreshToken
+        return done(null, profile)
+      })
+    }))
+app.use(cookieParser())
+app.use(bodyParser())
+app.use(methodOverride())
 app.use(session({
-    secret: 'ASdasdfasAroberttdfgdfg'
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+  secret: 'ASdasdfasAroberttdfgdfg'
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use(express.static(__dirname + '/public')); //serve static assets
-app.use(express.static(__dirname + '/node_modules')); //serve static assets
+app.use(express.static(path.join(__dirname, '/public'))) // serve static assets
+app.use(express.static(path.join(__dirname, '/node_modules'))) // serve static assets
 
-router.get("/", function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
-router.get("/load", function (req, res) {
-    if (req.query.secret != config.client_secret) {
-        res.status(666);
-        res.send("Unauthorized");
-    } else {
-        loader.load();
-        res.redirect("/loadstatus");
-    }
-
-
-});
-router.get("/loadstatus", function (req, res) {
-
-    if (loader.status.err) {
-        res.status(500);
-    }
-    res.json(loader.status);
-
-
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 })
-router.get("/tracks", function (req, res) {
-    var trackIds = req.query.tracks;
-    if (!(trackIds instanceof Array)) {
-        trackIds = [trackIds];
+router.get('/load', function (req, res) {
+  if (req.query.secret !== config.client_secret) {
+    res.status(666)
+    res.send('Unauthorized')
+  } else {
+    loader.load()
+    res.redirect('/loadstatus')
+  }
+})
+router.get('/loadstatus', function (req, res) {
+  loader.loadStatus().then(function (status) {
+    if (status.err) {
+      res.status(500)
     }
-    spotify.rootApi.refresh()
+    res.json(status)
+  })
+})
+router.get('/tracks', function (req, res) {
+  var trackIds = req.query.tracks
+  if (!(trackIds instanceof Array)) {
+    trackIds = [trackIds]
+  }
+  spotify.rootApi.refresh()
         .then(function () {
-            return spotify.rootApi.getTracks(trackIds);
+          return spotify.rootApi.getTracks(trackIds)
         })
         .then(function (response) {
-                res.json(response.body.tracks);
-            },
+          res.json(response.body.tracks)
+        },
             function (err) {
-                console.log(err);
-            });
-});
-router.get("/search", function (req, res) {
-    var query = req.query.query;
-    console.log("searching for ", query);
-    return MongoClient.connect(config.db_url)
+              console.log(err)
+            })
+})
+router.get('/search', function (req, res) {
+  var query = req.query.query
+  console.log('searching for ', query)
+  return MongoClient.connect(config.db_url)
         .then(function (db) {
-            return db.collection("playlist-names").find({
-                names: {
-                    $regex: query,
-                    $options: 'i'
-                }
-            }).limit(5).toArray();
+          return db.collection('playlist-names').find({
+            names: {
+              $regex: query,
+              $options: 'i'
+            }
+          }).limit(5).toArray()
         })
         .then(function (results) {
-            res.json(results);
+          res.json(results)
         }, function (err) {
-            console.log(err);
-        });
-});
-router.get("/history", function (req, res) {
-    var id = req.query.id;
-    console.log("searching history  for ", id);
-    return MongoClient.connect(config.db_url)
+          console.log(err)
+        })
+})
+router.get('/history', function (req, res) {
+  var id = req.query.id
+  console.log('searching history  for ', id)
+  return MongoClient.connect(config.db_url)
         .then(function (db) {
-            return db.collection("playlists").find({
-                id: id
-            }).limit(20).sort({
-                date: -1
-            }).toArray();
+          return db.collection('playlists').find({
+            id: id
+          }).limit(20).sort({
+            date: -1
+          }).toArray()
         })
         .then(function (results) {
-            res.json(results);
+          res.json(results)
         }, function (err) {
-            console.log(err);
-        });
-});
+          console.log(err)
+        })
+})
 
 var auth = function (req, res, next) {
-    if (!req.isAuthenticated()) {
-        res.send(401)
-    } else {
-        next()
-    }
-};
+  if (!req.isAuthenticated()) {
+    res.send(401)
+  } else {
+    next()
+  }
+}
 
 app.get('/login',
     passport.authenticate('spotify', {
-        scope: ['user-read-email', 'user-read-private', 'playlist-modify-private', 'playlist-modify-public']
+      scope: ['user-read-email', 'user-read-private', 'playlist-modify-private', 'playlist-modify-public']
     }),
-    function (req, res) {});
+    function (req, res) {})
 
 app.get('/callback',
     passport.authenticate('spotify', {
-        failureRedirect: '/'
+      failureRedirect: '/'
     }),
     function (req, res) {
-        res.redirect('/');
-    });
+      res.redirect('/')
+    })
 
 app.get('/logout', auth, function (req, res) {
-    req.logout();
-    res.redirect('/');
-});
+  req.logout()
+  res.redirect('/')
+})
 
 app.get('/user', function (req, res) {
-    res.send(req.isAuthenticated() ? req.user : false);
-});
+  res.send(req.isAuthenticated() ? req.user : false)
+})
 
 app.get('/restore', auth, function (req, res) {
-    var id = req.query.id;
-    var api = spotify.newApi();
-    var token = userTokens[req.user.id];
-    var name = req.query.name;
-    api.setRefreshToken(token);
-    MongoClient.connect(config.db_url)
+  var id = req.query.id
+  var api = spotify.newApi()
+  var token = userTokens[req.user.id]
+  var name = req.query.name
+  api.setRefreshToken(token)
+  MongoClient.connect(config.db_url)
         .then(function (db) {
-            return db.collection("playlists").findOne({
-                id: id
-            });
+          return db.collection('playlists').findOne({
+            id: id
+          })
         }).then(function (snapshot) {
-            return api.refresh()
+          return api.refresh()
                 .then(function () {
-                    return api.createPlaylist(req.user.id, name, {
-                        'public': false
-                    });
+                  return api.createPlaylist(req.user.id, name, {
+                    'public': false
+                  })
                 }).then(function (newPlaylist) {
-                    console.log("added playlist", newPlaylist.body.id);
-                    var offset = 0;
-                    return function addChunk() {
-                        var chunk = snapshot.tracks.slice(offset, offset + 50)
+                  console.log('added playlist', newPlaylist.body.id)
+                  var offset = 0
+
+                  function addChunk () {
+                    var chunk = snapshot.tracks.slice(offset, offset + 50)
                             .map(function (trackId) {
-                                return 'spotify:track:' + trackId;
-                            });
-                        console.log("adding chunk", offset);
-                        return api.addTracksToPlaylist(req.user.id, newPlaylist.body.id, chunk)
-                            .then(function () {
-                                console.log("added chunk", offset);
-                                offset += 50;
-                                if (offset < snapshot.tracks.length) {
-                                    return addChunk();
-                                } else {
-                                    res.send(0);
-                                }
-                            }, function (err) {
-                                console.log('Something went wrong!', err);
+                              return 'spotify:track:' + trackId
                             })
-                    }();
-                });
+                    console.log('adding chunk', offset)
+                    return api.addTracksToPlaylist(req.user.id, newPlaylist.body.id, chunk)
+                            .then(function () {
+                              console.log('added chunk', offset)
+                              offset += 50
+                              if (offset < snapshot.tracks.length) {
+                                return addChunk()
+                              } else {
+                                res.send(0)
+                              }
+                            }, function (err) {
+                              console.log('Something went wrong!', err)
+                            })
+                  };
+                  return addChunk()
+                })
         }, function (err) {
-            console.log('Something went wrong!', err);
+          console.log('Something went wrong!', err)
         })
+})
 
+app.use('/', router)
 
-});
-
-app.use("/", router);
-
-app.use("*", function (req, res) {
-    res.status(404);
-    res.sendFile(__dirname + "/public/404.html");
-});
+app.use('*', function (req, res) {
+  res.status(404)
+  res.sendFile(path.join(__dirname, '/public/404.html'))
+})
 
 app.listen(config.port, function () {
-    console.log("Live at Port", config.port);
-});
+  console.log('Live at Port', config.port)
+})
--- loader.js
+++ loader.js
@@ -1,130 +1,127 @@
-/*jslint node: true, nomen: true*/
-'use strict';
-var Promise = require('promise');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var config = require('./config');
-var spotify = require('./spotify');
+/* jslint node: true, nomen: true*/
+'use strict'
+var Promise = require('promise')
+var MongoClient = require('mongodb').MongoClient
+var config = require('./config')
+var spotify = require('./spotify')
 
-function filterNew(playlists) {
-    console.log('filtering');
-    return MongoClient.connect(config.db_url)
+function filterNew (playlists) {
+  console.log('filtering')
+  return MongoClient.connect(config.db_url)
         .then(function (db) {
-            return db.collection('playlists').find({
-                $or: playlists.map(function (playlist) {
-                    return {
-                        'id': playlist.id,
-                        'snapshot_id': playlist.snapshot_id
-                    };
-                })
-            }, {
-                id: 1
-            }).map(function (u) {
-                return u.id;
-            }).toArray().then(function (ids) {
-                var filtered = playlists.filter(function (playlist) {
-                    return ids.indexOf(playlist.id) === -1;
-                });
-                console.log('filtered', filtered.length);
-                return filtered;
-            });
-        });
+          return db.collection('playlists').find({
+            $or: playlists.map(function (playlist) {
+              return {
+                'id': playlist.id,
+                'snapshot_id': playlist.snapshot_id
+              }
+            })
+          }, {
+            id: 1
+          }).map(function (u) {
+            return u.id
+          }).toArray().then(function (ids) {
+            var filtered = playlists.filter(function (playlist) {
+              return ids.indexOf(playlist.id) === -1
+            })
+            console.log('filtered', filtered.length)
+            return filtered
+          })
+        })
 }
 
-function transformPlaylist(playlist) {
-    var transformed = {};
-    transformed.id = playlist.id;
-    transformed.name = playlist.name;
-    transformed.snapshot_id = playlist.snapshot_id;
-    transformed.owner = {
-        id: playlist.owner.id
-    };
-    transformed.date = new Date();
-    return transformed;
+function transformPlaylist (playlist) {
+  return {
+    id: playlist.id,
+    name: playlist.name,
+    snapshot_id: playlist.snapshot_id,
+    owner: {
+      id: playlist.owner.id
+    },
+    date: new Date()
+  }
 }
 
-function appendTracks(playlist, index, total) {
-    return spotify.all(spotify.rootApi.getPlaylistTracks, [playlist.owner.id, playlist.id], 100)
+function appendTracks (playlist, index, total) {
+  return spotify.all(spotify.rootApi.getPlaylistTracks, [playlist.owner.id, playlist.id], 100)
         .then(function (tracks) {
-            playlist.tracks = tracks.map(function (track) {
-                return track.track;
-            }).filter(function (track) {
-                return track !== null;
-            }).map(function (track) {
-                return track.id;
-            });
-            report('tracks' + index)(playlist.tracks);
-            console.log("extracted tracks", playlist.tracks.length);
-            console.log('extracted ', (index + 1), '/', total);
-            return playlist;
-        });
+          playlist.tracks = tracks.map(function (track) {
+            return track.track
+          }).filter(function (track) {
+            return track !== null
+          }).map(function (track) {
+            return track.id
+          })
+          report('tracks' + index)(playlist.tracks)
+          console.log('extracted tracks', playlist.tracks.length)
+          console.log('extracted ', (index + 1), '/', total)
+          return playlist
+        })
 }
 
-function insertPlaylists(playlists) {
-    return MongoClient.connect(config.db_url)
+function insertPlaylists (playlists) {
+  return MongoClient.connect(config.db_url)
         .then(function (db) {
-            if (playlists.length > 0) {
-                console.log('inserting', playlists.length);
-                return db.collection('playlists').insertMany(playlists).then(function (result) {
-                    var loaded = result.insertedCount;
-                    console.log("inserted: " + loaded);
-                    return loaded;
-                });
-            } else {
-                console.log("nothing to load");
-                return 0;
-            }
-
-        });
+          if (playlists.length > 0) {
+            console.log('inserting', playlists.length)
+            return db.collection('playlists').insertMany(playlists).then(function (result) {
+              var loaded = result.insertedCount
+              console.log('inserted: ' + loaded)
+              return loaded
+            })
+          } else {
+            console.log('nothing to load')
+            return 0
+          }
+        })
 }
 
-function aggregateNames() {
-    var aggregateCollName = "playlist-names";
-    console.log('aggregating names');
-    return MongoClient.connect(config.db_url)
+function aggregateNames () {
+  var aggregateCollName = 'playlist-names'
+  console.log('aggregating names')
+  return MongoClient.connect(config.db_url)
         .then(function (db) {
-            return db.collection(aggregateCollName).remove().then(function () {
-                console.log(aggregateCollName, " removed");
-                return db;
-            });
+          return db.collection(aggregateCollName).remove().then(function () {
+            console.log(aggregateCollName, ' removed')
+            return db
+          })
         })
         .then(function (db) {
-            return db.collection('playlists').aggregate([{
-                $group: {
-                    _id: "$id",
-                    id: {
-                        $first: "$id"
-                    },
-                    names: {
-                        $addToSet: "$name"
-                    }
-                }
-            }, {
-                $out: aggregateCollName
-            }]).toArray().then(function () {
-                console.log("aggregated");
-            });
-        });
+          return db.collection('playlists').aggregate([{
+            $group: {
+              _id: '$id',
+              id: {
+                $first: '$id'
+              },
+              names: {
+                $addToSet: '$name'
+              }
+            }
+          }, {
+            $out: aggregateCollName
+          }]).toArray().then(function () {
+            console.log('aggregated')
+          })
+        })
 }
 
-var start;
+var status
 
-function displayTime(arg) {
-    var diff = new Date() - start;
-    console.log(Math.floor(diff / 1000) + 's');
-    return arg;
+function displayTime (arg) {
+  var diff = new Date() - status.start
+  console.log(Math.floor(diff / 1000) + 's')
+  return arg
 }
-module.exports = {};
+module.exports = {}
 
 module.exports.load = function () {
-    start = new Date();
-    module.exports.status = {
-        finished: false
-    };
-    return spotify.rootApi.refresh()
+  status = {
+    finished: false,
+    start: new Date()
+  }
+  return spotify.rootApi.refresh()
         .then(function () {
-            return spotify.all(spotify.rootApi.getUserPlaylists, ['spotify']);
+          return spotify.all(spotify.rootApi.getUserPlaylists, ['spotify'])
         })
         .then(report('playlists'))
         .then(displayTime)
@@ -132,14 +129,14 @@ module.exports.load = function () {
         .then(report('filtered'))
         .then(displayTime)
         .then(function (playlists) {
-            console.log('extracted playlists: ' + Object.keys(playlists).length);
-            return Promise.all(playlists
+          console.log('extracted playlists: ' + Object.keys(playlists).length)
+          return Promise.all(playlists
                 .map(transformPlaylist)
                 .map(function (playlist, index, playlists) {
-                    return spotify.queue(function () {
-                        return appendTracks(playlist, index, playlists.length);
-                    });
-                }));
+                  return spotify.queue(function () {
+                    return appendTracks(playlist, index, playlists.length)
+                  })
+                }))
         })
         .then(displayTime)
         .then(insertPlaylists)
@@ -147,20 +144,38 @@ module.exports.load = function () {
         .then(aggregateNames)
         .then(displayTime)
         .then(function () {
-            console.log('finished');
+          console.log('finished')
         }, function (err) {
-            console.log(err);
-            module.exports.status.error = err;
+          console.log(err)
+          status.error = err
         })
-        .then(function () {
-            module.exports.status.finished = true;
-            console.log(module.exports.status);
-        });
-};
+        .then(storeStatus)
+}
 
-function report(type) {
-    return function (items) {
-        module.exports.status[type] = items.length;
-        return items;
-    }
+function storeStatus () {
+  status.finished = true
+  console.log(status)
+  return MongoClient.connect(config.db_url)
+        .then(function (db) {
+          return db.collection('statuses').insert(status)
+        })
+}
+module.exports.loadStatus = function () {
+  if (status) {
+    return Promise.resolve(status)
+  } else {
+    return MongoClient.connect(config.db_url)
+            .then(function (db) {
+              return db.collection('statuses').find().sort({
+                start: -1
+              }).next()
+            })
+  }
+}
+
+function report (type) {
+  return function (items) {
+    status[type] = items.length
+    return items
+  }
 }
